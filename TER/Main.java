c726878c91a021f2e8cd1189a22b050829fe6762
martinoux <75212042+martinoux@users.noreply.github.com>
2023-04-24 11:07:30 +0200

    Update Main.java

--- TER/Main.java
+++ TER/Main.java
@@ -1,17 +1,36 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
     public static void main (String[] args) {
-        Graph g = new Graph(2);
+        writeSudokuIntoFile(3);
+        
+    }
+
+    public static void writeSudokuIntoFile (int size) {
+        Graph g = new Graph(size);
         g.remplirRoot();
-        g.printValue();
-        
-       g.permutationAleatoire();
-        System.out.println();
-            g.printValue();
-    
-        g.emptySudoku(0.5);
-        System.out.println();
-        g.printValue();
-        //g.printAdjList();
-        
+        for (int i = 1; i<10; i++) {
+            try {
+                FileWriter writer = new FileWriter("Sudoku/" + size +"_"+String.valueOf((int) (i)));
+                g.permutationAleatoire();
+
+                Graph g2 = new Graph(g);
+                g2.emptySudoku((double)i /10);
+                
+                writer.write(size + "\n");
+                for (int row = 0; row < size*size; row++) {
+                    for (int col = 0; col < size*size; col++) {
+                        writer.write(g2.puzzle[row][col] + " ");
+                    }
+                    writer.write("\n");
+                }
+                writer.close();
+                System.out.println("fini");
+            } catch (IOException e) {
+                System.out.println("Error writing sudoku to file ");
+                e.printStackTrace();
+            }
+        }
     }
 }   
